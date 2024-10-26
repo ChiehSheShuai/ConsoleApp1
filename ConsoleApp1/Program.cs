a021b5376d766fb5f8b8d6a028c0665d8cb5aab3
ChiehSheShuai <ChiehSheShuai>
2024-10-26 16:49:35 +0300

    if (==)       {       Console.WriteLine("\nalt satıra geç");       } else { }
    Console.ReadLine();

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,9 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-                Console.WriteLine("hellöğ");
-            Console.WriteLine("\nwöğrd");
-            Console.WriteLine("commit1");
+            Console.WriteLine("*** İsim doğrulama programı ***\n\n");
+            Console.Write("sorgulamak istediğiniz ismi giriniz: ");
+            string a = Console.ReadLine();
+            if (a == "Mehmet")
+            { Console.WriteLine("\naranan isim doğru"); }
+            else
+            {
+                Console.WriteLine("\nbu isim bulunmamakta");
+            }
+            Console.Write("sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi < 0) { Console.WriteLine("sayınız negatif"); } //sıfır pozitif olsun
+            else { Console.WriteLine("sayınız {0}pozitif", sayi); }
+
+            Console.ReadLine();
         }
     }
 }
